50a9ae661e27eedefefacb416504abeec7e569a9
Drew Derbyshire <ahd@kew.com>
1992-11-28 23:08:07 +0000

    Rewrite into REXX for OS/2

--- scripts/uuclean.cmd
+++ scripts/uuclean.cmd
@@ -1,80 +1,300 @@
-echo off
-REM *------------------------------------------------------------------*
-REM *    Written and contributed by Michael Morrett                    *
-REM *    (ICBJ100@INDYVAX.IUPUI.EDU).  Support for by Snuffles P.      *
-REM *    Bear (Snuffles@kew.com).  Comments by Drew Derbyshire         *
-REM *    (help@kew.com); delete the comments to make this run faster.  *
-REM *------------------------------------------------------------------*
-REM *   Define your spool and temp directories on following line       *
-REM *------------------------------------------------------------------*
-if "%1" == "" %0 \uupc\spool %temp%
-if "%2" == "" %0 %1 %temp%
-REM *------------------------------------------------------------------*
-REM *        Handle recursive call to process one directory            *
-REM *------------------------------------------------------------------*
-if "%1" == "cleanit" goto cleanit:
-echo Cleaning UUPC/extended directories %1 and %2
-REM *------------------------------------------------------------------*
-REM *    Cruel hack for kendra's automatic sig file generation;        *
-REM *                other systems will ignore this                    *
-REM *------------------------------------------------------------------*
-if exist \os2bin\newsigs.cmd call newsigs
-REM *------------------------------------------------------------------*
-REM *     Temporary files created by UUCICO in spool directory         *
-REM *------------------------------------------------------------------*
-if exist %1\*.$$$ erase %1\*.$$$
-if exist %1\*.tmp erase %1\*.tmp
-REM *------------------------------------------------------------------*
-REM * Temporary files created by MAIL and RMAIL.  (and their           *
-REM * backups created by many editors)                                 *
-REM *------------------------------------------------------------------*
-if exist %2\UUPC*.tmp erase %2\UUPC*.tmp
-if exist %2\UUPC*.txt erase %2\UUPC*.txt
-if exist %2\UUPC*.bak erase %2\UUPC*.bak
-REM *------------------------------------------------------------------*
-REM *                 Clean the various log files                      *
-REM *------------------------------------------------------------------*
-call %0 cleanit %1\linedata
-call %0 cleanit %1\rmail
-call %0 cleanit %1\rnews
-call %0 cleanit %1\uuxqt
-call %0 cleanit %1\uucico
-call %0 cleanit %1\expire
-if exist %1\syslog     rename %1\syslog syslog.log
-call %0 cleanit %1\syslog
-call %0 cleanit %1\generic
-REM *------------------------------------------------------------------*
-REM *            Archive and delete temporary log files                *
-REM *------------------------------------------------------------------*
-if not exist %1\*.log goto noneleft
-copy   %1\*.log %1\generic.lgs
-erase  %1\*.log
-rename %1\generic.lgs generic.log
-:noneleft
-REM *------------------------------------------------------------------*
-REM *                      Clean up old news                           *
-REM *------------------------------------------------------------------*
-expire
-endlocal
-REM *------------------------------------------------------------------*
-REM *         Purge any files we killed from the undelete bucket.      *
-REM *         This applies to OS/2 2.0 only.                           *
-REM *------------------------------------------------------------------*
-undelete %1 /a /s /f
-undelete %2 /a /s /f
-goto exit
-REM *------------------------------------------------------------------*
-REM *                  Clean up a single log file                      *
-REM *------------------------------------------------------------------*
-:cleanit
-if not exist %2.log goto :exit
-echo Cleaning %2.log files.
-if exist %2.004    del %2.004
-if exist %2.003 rename %2.003 *.004
-if exist %2.002 rename %2.002 *.003
-if exist %2.001 rename %2.001 *.002
-if exist %2.log rename %2.log *.001
-REM *------------------------------------------------------------------*
-REM *                        All done, exit                            *
-REM *------------------------------------------------------------------*
-:exit
+/*--------------------------------------------------------------------*/
+/*       Program: UUCLEAN.CMD          28 Nov 1992                    */
+/*        Author: Andrew H. Derbyshire                                */
+/*                P.O. Box 132                                        */
+/*                Arlington, MA 01274                                 */
+/*      Internet: help@kew.com                                        */
+/*      Function: Perform automatic log file aging and purging        */
+/*                for UUPC/extended.                                  */
+/*      Language: REXX for OS/2 2.0                                   */
+/*     Arguments: None                                                */
+/*   Environment: OS/2 environment variable must be set to the        */
+/*                location of the UUPC/extended system                */
+/*                configuation file                                   */
+/*                (SET UUPCSYSRC=C:\UUPC\UUPC.RC)                     */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*    Copyright (c) 1992 by Kendra Electronic Wonderworks, all        */
+/*    rights reserved except those granted by the UUPC/extended       */
+/*    license.                                                        */
+/*--------------------------------------------------------------------*/
+
+/*
+ *       $Id$
+ *
+ *       $Log$
+ */
+
+/*--------------------------------------------------------------------*/
+/*                    Trap uninitialized variables                    */
+/*--------------------------------------------------------------------*/
+
+signal on novalue
+'@echo off'                   /* Do not echo command input           */
+
+/*--------------------------------------------------------------------*/
+/*    To do anything, we need the name of the UUPC/extended spool     */
+/*    directory, configuration directory, and temporary directory     */
+/*--------------------------------------------------------------------*/
+
+spooldir = getuupc("SPOOLDIR" )
+if spooldir == '' then
+do
+   say 'No spool directory defined, cannot continue'
+   exit 99
+end
+
+confdir = getuupc("CONFDIR" )
+if confdir == '' then
+do
+   say 'No configuration directory defined, cannot continue'
+   exit 98
+end
+
+tempdir = getuupc("TEMPDIR" )
+if tempdir == '' then
+   tempdir = value('TEMP',,'OS2ENVIRONMENT')
+if tempdir == '' then
+   tempdir = value('TMP',,'OS2ENVIRONMENT')
+if tempdir == '' then
+do
+   say 'No TEMP directory defined, cannot continue'
+   exit 98
+end
+
+/*--------------------------------------------------------------------*/
+/*                    Disable UNDELETE processing                     */
+/*--------------------------------------------------------------------*/
+
+deldir = value('DELDIR','','OS2ENVIRONMENT')
+
+/*--------------------------------------------------------------------*/
+/*    Process odd logfiles which may have been left around by         */
+/*    aborted programs.                                               */
+/*--------------------------------------------------------------------*/
+
+call process spooldir,'UUPC*.LOG', 'GENERIC'
+
+/*--------------------------------------------------------------------*/
+/*             SYSLOG has funny name, so process explictly            */
+/*--------------------------------------------------------------------*/
+
+call process spooldir,'SYSLOG', 'SYSLOG'
+
+/*--------------------------------------------------------------------*/
+/*                     Process all other log files                    */
+/*--------------------------------------------------------------------*/
+
+xrc = SysFileTree(spooldir || '\*.log', 'data.','F')
+if xrc == 0 then
+do count = 1 to data.0
+   parse upper var data.count mmddyy hhmmss bytes attr fname
+   basename = filespec( 'N' , data.count )
+   parse var basename stem'.'
+   if left( basename, 4 ) <> 'UUPC' then   /* Don't do UUPC*.LOG again */
+      call process spooldir, basename, stem
+end
+
+/*--------------------------------------------------------------------*/
+/*           Clean up temporary files in the spool directory          */
+/*--------------------------------------------------------------------*/
+
+call purge spooldir, '*.TMP'     /* Created by UUCICO                */
+call purge spooldir, '*.BAK'     /* Maybe created by UUCICO          */
+
+/*--------------------------------------------------------------------*/
+/*         Clean up temporary files in the Temporary directory        */
+/*--------------------------------------------------------------------*/
+
+call purge tempdir,'*.BAK'
+call purge tempdir,'UUPC*.TMP'
+call purge tempdir,'UUPC*.TXT'
+
+/*--------------------------------------------------------------------*/
+/*             If the news ACTIVE file exists, run expire             */
+/*--------------------------------------------------------------------*/
+
+if exist( confdir || '\active' ) then
+   'expire'
+
+
+/*--------------------------------------------------------------------*/
+/*     Re-enable UNDELETE processing so we can clean up the cache     */
+/*--------------------------------------------------------------------*/
+
+call value 'DELDIR','deldir','OS2ENVIRONMENT'
+
+/*--------------------------------------------------------------------*/
+/*    Purge the undelete cache to improve performance and free        */
+/*    space                                                           */
+/*--------------------------------------------------------------------*/
+
+if deldir <> '' then
+do;
+   'UNDELETE /F /S /A' spooldir;
+   'UNDELETE /F /S /A' tempdir;
+end;
+
+/*--------------------------------------------------------------------*/
+/*                           All done, exit                           */
+/*--------------------------------------------------------------------*/
+
+return
+
+/*--------------------------------------------------------------------*/
+/*    p r o c e s s                                                   */
+/*                                                                    */
+/*    Age a single set of log files                                   */
+/*--------------------------------------------------------------------*/
+
+process:procedure
+parse upper arg spooldir, input, archive, maxsize, generation
+maxgen = 5
+aged   = 0                    /* Next older version was aged         */
+moved  = 0                    /* This version was aged               */
+if generation = '' then
+   generation = 1
+else
+   generation = generation + 1
+
+if maxsize = '' then
+do;
+   if pos('*', input) > 0 then
+      maxsize = 0
+   else
+      maxsize = 10000;
+end;
+newgen = archive || '.' || right( generation, 3, '0')
+target = spooldir || '\' || newgen
+
+if substr(target,2,1) = ':' then
+  target = substr( target, 3 );
+
+/*--------------------------------------------------------------------*/
+/*          Determine if file exists  if not, return quietly          */
+/*--------------------------------------------------------------------*/
+
+xrc = SysFileTree(spooldir || '\' || input , 'data.','F')
+if xrc <> 0 then
+   return 0
+/*--------------------------------------------------------------------*/
+/*             Process whatever files the search turned up            */
+/*--------------------------------------------------------------------*/
+do count = 1 to data.0
+   data = space(data.count)
+   parse upper var data mmddyy hhmmss bytes attr fname
+   if bytes = 0 then             /* Kill any empty file we find      */
+      call Purge fname
+   else if bytes > maxsize then
+   do
+
+      if \ aged then             /* Only age olders file per run     */
+         aged = process(spooldir, newgen , archive, maxsize, generation)
+      say 'Aging' input 'to' target
+
+      if generation > maxgen then      /* Really old files go away   */
+         call Purge fname
+      else if exist( target ) then     /* Else append if needed      */
+      do
+         'COPY' target || '+' || fname
+         call SysFileDelete fname
+      end
+      else
+         'MOVE' fname target     /* But move if possible, faster     */
+      moved = 1
+   end
+
+end count
+
+/*--------------------------------------------------------------------*/
+/*       Flag whether or not files were moved to previous level       */
+/*--------------------------------------------------------------------*/
+
+return moved
+
+/*--------------------------------------------------------------------*/
+/*    p u r g e                                                       */
+/*                                                                    */
+/*    Delete files with specified mask and wildcard spec              */
+/*--------------------------------------------------------------------*/
+
+purge:procedure
+parse arg directory,file
+if file == '' then
+   mask = directory
+else
+   mask = directory || '\' || file
+xrc = SysFileTree(mask, 'dir.', 'SF', '*----')
+if xrc <> 0 then
+do
+   say 'Directory read error' xrc
+   exit xrc
+end
+if dir.0 > 1 then
+   say dir.0 'files found for' mask
+do count = 1 to dir.0
+   parse var dir.count mmddyy hhmmss bytes attr fname
+   fname = space( fname )
+   xrc = sysfiledelete( fname )
+   if xrc == 0 then
+      say 'Deleted' fname
+   else
+      say 'Error deleting' fname || ', return code =' xrc
+end
+return
+
+/*--------------------------------------------------------------------*/
+/*    e x i s t                                                       */
+/*                                                                    */
+/*    Report whether or not a file exists                             */
+/*--------------------------------------------------------------------*/
+exist: procedure
+parse arg file
+xrc = SysFileTree(file, 'data.','F')
+if data.0 == 0 then
+   return 0
+else
+   return 1
+
+/*--------------------------------------------------------------------*/
+/*       g e t u u p c                                                */
+/*                                                                    */
+/*       Get UUPC/extended configuration variable                     */
+/*--------------------------------------------------------------------*/
+
+getuupc:procedure
+parse upper arg keyword,answer
+
+uupcrc = value('UUPCSYSRC',,'OS2ENVIRONMENT')
+if  uupcrc == '' then
+do
+   'UUPCSYSRC not set, cannot continue'
+   exit 44
+end
+
+xrc = SysFileSearch( keyword || '=',uupcrc,'data.')
+if xrc \= 0 then
+do
+   say 'SysFileSearch error' xrc 'searching' uupcrc 'for' keyword
+   exit xrc
+end
+
+do count = 1 to data.0
+   parse var data.count newkey'='string
+
+   if translate(newkey) = keyword then
+      answer = string
+end
+return translate(answer,'\','/')
+
+/*--------------------------------------------------------------------*/
+/*    n o v a l u e                                                   */
+/*                                                                    */
+/*    Trap for uninitialized variables                                */
+/*--------------------------------------------------------------------*/
+
+novalue:
+signal off novalue            /* Avoid nasty recursion         */
+say 'Uninitialized variable in line' sigl || ':'
+say sourceline( sigl )
