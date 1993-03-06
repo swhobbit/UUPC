a5c9b6958bb6b37ef384ea929e2a4ae9a8a96be5
Drew Derbyshire <ahd@kew.com>
1993-03-06 23:09:50 +0000

    Break variable names out of master lib.h

--- lib/lib.h
+++ lib/lib.h
@@ -1,17 +1,5 @@
 /*--------------------------------------------------------------------*/
 /*      l i b . h                                                     */
-/*                                                                    */
-/*      Update log:                                                   */
-/*                                                                    */
-/*      13 May 89    Added PCMAILVER                        ahd       */
-/*      Summer 89    Added equali, equalni, compiled,                 */
-/*                         compilet                         ahd       */
-/*      22 Sep 89    Add boolean typedef                    ahd       */
-/*      01 Oct 89    Make logecho boolean                   ahd       */
-/*      19 Mar 90    Move FOPEN prototype to here           ahd       */
-/*      02 May 1990  Allow set of booleans options via options=       */
-/*  8 May  90  Add 'pager' option                                     */
-/* 10 May  90  Add 'purge' option                                     */
 /*--------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------*/
@@ -29,10 +17,13 @@
 /*--------------------------------------------------------------------*/
 
 /*
- *    $Id: lib.h 1.2 1992/11/22 21:31:22 ahd Exp ahd $
+ *    $Id: LIB.H 1.3 1992/12/01 04:39:34 ahd Exp ahd $
  *
  *    Revision history:
- *    $Log: lib.h $
+ *    $Log: LIB.H $
+ *     Revision 1.3  1992/12/01  04:39:34  ahd
+ *     Add SpeedOverMemory
+ *
  * Revision 1.2  1992/11/22  21:31:22  ahd
  * Allow strpool() to allocate memory for const strings
  *
@@ -43,6 +34,8 @@
 
 #ifndef __LIB
 #define __LIB
+
+#include <confvars.h>
 
 /*--------------------------------------------------------------------*/
 /*     Environment variables set by UUXQT before invoking modules     */
@@ -101,7 +94,7 @@
 #define B_UUXQT    0x00001000L /* Used by queue processor UUXQT       */
 #define B_INSTALL  0x00002000L /* Used by install program only        */
 #define B_MAIL     (B_MUA | B_MTA | B_MUSH)
-#define B_SPOOL    (B_MTA | B_UUCICO | B_UUXQT | B_UUCP | B_UUSTAT)
+#define B_SPOOL    (B_MTA | B_NEWS | B_UUCICO | B_UUXQT | B_UUCP | B_UUSTAT)
 #define B_ALL      (B_MAIL|B_SPOOL|B_NEWS|B_UUPOLL|B_UUSTAT)
 
 #define B_INTEGER  0x00010000L /* Pointer is to short int, not string */
@@ -160,95 +153,6 @@
 #define nil(type)               ((type *)NULL)
 
 /*--------------------------------------------------------------------*/
-/*    Enumerated list used by the options= configuration line         */
-/*--------------------------------------------------------------------*/
-
-typedef enum {
-
-/*--------------------------------------------------------------------*/
-/*                          Per user options                          */
-/*--------------------------------------------------------------------*/
-
-               F_ASKCC,       /* TRUE = ask for Carbon copies              */
-               F_DOSKEY,      /* TRUE = Use DOSKEY under DOS 5 if available*/
-               F_BACKUP,      /* TRUE = Backup mailbox before rewriting    */
-               F_DOT,         /* TRUE = period ends a message              */
-               F_AUTOPRINT,   /* TRUE = print next message automatically   */
-               F_AUTOEDIT,    /* TRUE = no line prompt, always edit        */
-               F_AUTOINCLUDE, /* TRUE = Perform automatic INCLUDE command  */
-               F_AUTOSIGN,    /* TRUE = append the signature file          */
-               F_EXPERT,      /* TRUE = Let user shoot self in foot w/o
-                                        a message.                         */
-               F_FROMSEP,     /* TRUE = Allow From to split messages       */
-               F_PAGER,       /* TRUE = Invert meaning of P/p T/t commands */
-               F_PURGE,       /* TRUE = Delete mailbox, if empty           */
-               F_SAVE,        /* TRUE = Save read messages in =mbox when
-                                        reading new mail.                  */
-               F_SAVERESENT,  /* TRUE = Save forwarded mail a second time  */
-               F_SUPPRESSCOPYRIGHT,
-                              /* Skip copyright message                    */
-               F_SPEEDOVERMEMORY,
-                              /* TRUE = Be lazy in strpool()               */
-               F_VERBOSE,     /* TRUE = Verbose RMAIL output               */
-
-/*--------------------------------------------------------------------*/
-/*                    Per system (GLOBAL) options                     */
-/*--------------------------------------------------------------------*/
-
-               F_BANG,        /* TRUE = re-write addresses in bang (!) form*/
-               F_COLLECTSTATS,/* TRUE = Report additional information in
-                                        various logs                       */
-               F_DIRECT,      /* TRUE = Deliver to subdirectories, not
-                                        files                              */
-               F_ESCAPE,      /* TRUE = Allow ESCAPE to act as Ctrl-Break  */
-               F_HPFS,        /* TRUE = Exploit HPFS names under OS/2      */
-               F_HISTORY,     /* TRUE = Maintain history of of articles
-                                        read and posted                    */
-               F_KANJI,       /* TRUE = enable Kanji (Japanese) support    */
-               F_MULTI,       /* TRUE = Deliver to multiple addresses on
-                                        remote host at once                */
-               F_MULTITASK,   /* TRUE = System is multitasking, watch
-                                        for race conditions                */
-               F_ONECASE,     /* TRUE = Remote host is case insensitive    */
-               F_SNEWS,       /* TRUE = Use Simple news delivery           */
-               F_SYMMETRICGRADES,
-                              /* TRUE = Use send grade as receive grade    */
-               F_SYSLOG,      /* TRUE = Write syslog with name and time of
-                                        each file transferred              */
-               F_UNDELETE,    /* TRUE = Do not override OS/2 undelete
-                                 support                                   */
-               F_LAST }       /* Dummy - Must be last - defines array size */
-               B_FLAG;        /* Boolean flag array - ahd                  */
-
-/*--------------------------------------------------------------------*/
-/*                  Your basic Boolean logic values                   */
-/*--------------------------------------------------------------------*/
-
-#undef FALSE
-#undef TRUE
-typedef enum { FALSE = 0, TRUE = 1 } boolean;
-
-/*--------------------------------------------------------------------*/
-/*                      Configuration mode flags                      */
-/*--------------------------------------------------------------------*/
-
-typedef enum {
-      USER_CONFIG,
-      SYSTEM_CONFIG,
-      MODEM_CONFIG
-      } SYSMODE;
-
-/*--------------------------------------------------------------------*/
-/*               enumerated type for configuration bits               */
-/*--------------------------------------------------------------------*/
-
-typedef unsigned long CONFIGBITS;
-
-typedef unsigned short INTEGER;  /* Integers in the config file      */
-
-typedef unsigned int BPS;
-
-/*--------------------------------------------------------------------*/
 /*                     Configuration table layout                     */
 /*--------------------------------------------------------------------*/
 
@@ -284,34 +188,6 @@ struct file_queue {
 extern int debuglevel;
 extern FILE *logfile;
 extern boolean bflag[F_LAST];
-
-/*--------------------------------------------------------------------*/
-/*      Configuration file strings                                    */
-/*--------------------------------------------------------------------*/
-
-extern char *E_name, *E_mailbox, *E_homedir;
-extern char *E_archivedir, *E_newsserv;                        /* news */
-extern char *E_mailbox, *E_homedir;
-extern char *E_maildir, *E_newsdir, *E_spooldir, *E_confdir;
-extern char *E_pubdir, *E_tempdir;
-extern char *E_nodename, *E_domain, *E_mailserv;
-extern char *E_fdomain;       /* Domain used in From: lines             */
-extern char *E_inmodem;
-extern char *E_editor, *E_pager;
-extern char *E_mailext, *E_backup;
-extern char *E_filesent, *E_signature;
-extern char *E_altsignature;                                   /* pdm   */
-extern char *E_aliases;                                        /* ahd   */
-extern char *E_organization, *E_replyto;                       /* ahd   */
-extern char *E_localdomain;                                    /* ahd   */
-extern char *E_postmaster;                                     /* ahd   */
-extern char *E_uuxqtpath;
-extern char **E_internal;
-extern char *E_charset;
-extern char *E_anonymous;
-extern char *E_version;
-extern char *E_uncompress;
-extern INTEGER E_maxhops;
 
 extern FLAGTABLE configFlags[];
 
@@ -384,6 +260,7 @@ extern   char *copyright;
 extern   char *copywrong;
 extern   long *lowcore;
 #endif   /* defined(__CORE__)   */
-#endif   /* ifndef __LIB */
 
 #define msdos                 /* Required by dain bramaged compiler  */
+
+#endif   /* ifndef __LIB */
