cbfeb2ff32eb67ac15a409321513b910c81e7d58
Drew Derbyshire <ahd@kew.com>
1993-05-04 00:25:58 +0000

    Support personal.rc to allow su.cmd

--- scripts/getuupc.cmd
+++ scripts/getuupc.cmd
@@ -11,17 +11,24 @@
 /*--------------------------------------------------------------------*/
 
 /*
- *       $Id$
+ *       $Id: getuupc.cmd 1.1 1993/04/04 05:01:49 ahd Exp $
  *
- *       $Log$
+ *       $Log: getuupc.cmd $
+*        Revision 1.1  1993/04/04  05:01:49  ahd
+*        Initial revision
+*
  */
 
-parse upper arg keyword,answer
+signal on novalue
+trace i
+parse upper arg keyword,answer,uupcusrc
 
 /*--------------------------------------------------------------------*/
 /*                     Get the UUPC.RC file name                      */
 /*--------------------------------------------------------------------*/
+
 uupcrc = value('UUPCSYSRC',,'OS2ENVIRONMENT')
+uupcusrc = value('UUPCUSRRC',,'OS2ENVIRONMENT')
 if  uupcrc == '' then
 do
    'UUPCSYSRC not set, cannot continue'
@@ -30,6 +37,22 @@ end
 
 confdir = translate(filespec('D',uupcrc) || filespec('P',uupcrc),'\','/');
 
+answer = search( keyword, answer, confdir, uupcrc );
+if uupcusrc <> '' then
+do;
+   uupcusrc = translate( uupcusrc , '\', '/');
+
+   if verify( uupcusrc, '\/', 'N') == 0 then
+      uupcusrc = confdir || '\' || uupcusrc;
+
+   answer = search( keyword, answer, confdir, uupcusrc );
+end;
+if left(answer,1) == '\' then
+   answer = filespec('D',uupcrc) || answer;
+return answer;
+
+search:procedure
+parse arg keyword,answer,confdir,uupcrc
 /*--------------------------------------------------------------------*/
 /*     Read the file, then scope the contents a line at the time      */
 /*--------------------------------------------------------------------*/
@@ -48,6 +71,7 @@ do count = 1 to data.0
       answer = string
 end
 
+parse arg keyword,answer,confdir
 /*--------------------------------------------------------------------*/
 /*    Return the value if found, return directories if we can         */
 /*    generate the directory name, otherwise return default input.    */
