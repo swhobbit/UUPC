637ea129528a67203311e1412660c620d927a86d
Drew Derbyshire <ahd@kew.com>
1993-05-30 00:11:03 +0000

    Multiple communications drivers support

--- uucico/ulib.h
+++ uucico/ulib.h
@@ -1,28 +1,51 @@
+#ifndef ULIB_H
+#define ULIB_H
+
+/*--------------------------------------------------------------------*/
+/*    u l i b . h                                                     */
+/*                                                                    */
+/*    Native mode (serial port) communications functions              */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*    Changes Copyright (c) 1990-1993 by Kendra Electronic            */
+/*    Wonderworks.                                                    */
+/*                                                                    */
+/*    All rights reserved except those explicitly granted by the      */
+/*    UUPC/extended license agreement.                                */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*                          RCS Information                           */
+/*--------------------------------------------------------------------*/
+
 /*
-   ulib.h
-*/
+ *    $Id: lib.h 1.5 1993/04/04 21:51:00 ahd Exp $
+ *
+ *    Revision history:
+ *    $Log: lib.h $
+ */
 
+extern int nopenline(char *name, BPS baud, const boolean direct);
 
-extern boolean port_active;         /* Port active flag for error handler   */
-
-extern int openline(char *name, BPS baud, const boolean direct);
-
-extern unsigned int sread(char *buffer,
+extern unsigned int nsread(char *buffer,
                           unsigned int wanted,
                           unsigned int timeout);
 
-int swrite(const char *data, unsigned int len);
+int nswrite(const char *data, unsigned int len);
 
-void ssendbrk(unsigned int duration);
+void nssendbrk(unsigned int duration);
 
-void closeline(void);
+void ncloseline(void);
 
-void SIOSpeed(BPS baud);
+void nSIOSpeed(BPS baud);
 
-void flowcontrol( boolean );
+void nflowcontrol( boolean );
 
-void hangup( void );
+void nhangup( void );
 
-BPS GetSpeed( void );
+BPS nGetSpeed( void );
 
-boolean CD( void );
+boolean nCD( void );
+
+#endif
