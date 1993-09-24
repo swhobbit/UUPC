116174d26d3da9a0fe5152ee39b47407b51be121
Drew Derbyshire <ahd@kew.com>
1993-09-24 03:43:27 +0000

    General bug fixes to make work

--- uucico/ulibnmp.c
+++ uucico/ulibnmp.c
@@ -17,8 +17,11 @@
 /*--------------------------------------------------------------------*/
 
 /*
- *       $Id: ulibos2.c 1.16 1993/09/21 01:42:13 ahd Exp $
- *       $Log: ulibos2.c $
+ *       $Id: ulibpnm.c 1.1 1993/09/23 03:26:51 ahd Exp $
+ *       $Log: ulibpnm.c $
+ * Revision 1.1  1993/09/23  03:26:51  ahd
+ * Initial revision
+ *
  *
  */
 
@@ -51,11 +54,12 @@ typedef USHORT APIRET ;  // Define older API return type
 /*--------------------------------------------------------------------*/
 
 #include "lib.h"
-#include "ulib.h"
+#include "ulibnmp.h"
 #include "ssleep.h"
 #include "catcher.h"
 
 #include "commlib.h"
+#include "pos2err.h"
 
 /*--------------------------------------------------------------------*/
 /*                          Global variables                          */
@@ -69,13 +73,15 @@ static boolean hangupNeeded = FALSE;
 
 static currentSpeed = 0;
 
+static boolean passive;
+
 #define FAR_NULL ((PVOID) 0L)
 
 /*--------------------------------------------------------------------*/
 /*           Definitions of control structures for DOS API            */
 /*--------------------------------------------------------------------*/
 
-static HFILE com_handle;
+static HPIPE pipeHandle;
 
 #ifdef __OS2__
 static ULONG usPrevPriority;
@@ -84,49 +90,138 @@ static USHORT usPrevPriority;
 #endif
 
 /*--------------------------------------------------------------------*/
-/*    p o p e n l i n e                                               */
+/*    p p a s s i v e o p e n l i n e                                 */
 /*                                                                    */
-/*    Open the serial port for I/O                                    */
+/*    Open a server pipe connection                                   */
 /*--------------------------------------------------------------------*/
 
-int popenline(char *name, BPS baud, const boolean direct )
+#ifdef __TURBOC__
+#pragma argsused
+#endif
+
+int ppassiveopenline(char *name, BPS baud, const boolean direct )
 {
 
    APIRET rc;
-   USHORT com_error;
 
-#ifdef __OS2__
-   ULONG ParmLengthInOut;
-   ULONG DataLengthInOut;
-
-   PTIB ptib;
-   PPIB ppib;
-
-   ULONG action;
-
-#else
-   USHORT action;
-#endif
-
-
-   USHORT priority = (E_priority == -99) ?
-                           PRTYC_FOREGROUNDSERVER : (USHORT) E_priority;
-   USHORT prioritydelta = (E_prioritydelta == -99) ?
-                           0 : (USHORT) (E_prioritydelta + PRTYD_MINIMUM);
-
-   if (portActive)                  /* Was the port already active?     */
+   if (portActive)                  /* Was the port already active?    */
       closeline();                  /* Yes --> Shutdown it before open */
 
 #ifdef UDEBUG
-   printmsg(15, "popenline: %s, %d", name, baud);
+   printmsg(15, "popenpassive: %s, %d", name, baud);
 #endif
 
 /*--------------------------------------------------------------------*/
 /*                          Perform the open                          */
 /*--------------------------------------------------------------------*/
 
+   rc =  DosCreateNPipe( "\\pipe\\uucp",
+                         &pipeHandle,
+                         NP_ACCESS_DUPLEX | NP_INHERIT | NP_NOWRITEBEHIND,
+                         NP_NOWAIT | 1,
+                         MAXPACK,
+                         MAXPACK,
+                         60 );
+
+/*--------------------------------------------------------------------*/
+/*    Check the open worked.  We translation the common obvious       */
+/*    error of file in use to english, for all other errors are we    */
+/*    report the raw error code.                                      */
+/*--------------------------------------------------------------------*/
+
+   if ( rc == ERROR_SHARING_VIOLATION)
+   {
+      printmsg(0,"Pipe %s already in use", name);
+      return TRUE;
+   }
+   else if ( rc )
+   {
+      printOS2error("DosCreateNPipe", rc );
+      return TRUE;
+   }
+
+/*--------------------------------------------------------------------*/
+/*                           Set baud rate                            */
+/*--------------------------------------------------------------------*/
+
+   SIOSpeed(57600);        // Just any old large number.
+
+   traceStart( name );     // Enable logging
+
+   portActive = TRUE;      /* Record status for error handler        */
+   passive    = TRUE;
+   carrierDetect = FALSE;  /* Modem is not connected                 */
+
+   return 0;
+
+} /* ppassiveopenline */
+
+/*--------------------------------------------------------------------*/
+/*       p W a i t F o r N e t C o n n e c t                          */
+/*                                                                    */
+/*       Wait for network connection                                  */
+/*--------------------------------------------------------------------*/
+
+boolean pWaitForNetConnect(int timeout)
+{
+
+   time_t stop;
+
+   stop  = time( NULL ) + timeout;
+
+   do {
+      APIRET rc = DosConnectNPipe( pipeHandle );
+
+      if ( rc == 0 )
+      {
+         hangupNeeded = TRUE;      /* Flag that the pipe is now dirty  */
+         return TRUE;
+      }
+      else if ( rc == ERROR_PIPE_NOT_CONNECTED )
+         ddelay(10);
+      else {
+         printOS2error("DosConnectNPipe", rc );
+         return FALSE;
+      } /* else */
+
+   } while( (stop > time( NULL )) && ! terminate_processing );
+
+   return FALSE;
+
+}  /* pWaitForNetConnect */
+
+/*--------------------------------------------------------------------*/
+/*    p a c t i v e o p e n l i n e                                   */
+/*                                                                    */
+/*    Open a client pipe connection                                   */
+/*--------------------------------------------------------------------*/
+
+#ifdef __TURBOC__
+#pragma argsused
+#endif
+
+int pactiveopenline(char *name, BPS baud, const boolean direct )
+{
+
+   APIRET rc;
+
+#ifdef __OS2__
+   ULONG action;
+#else
+   USHORT action;
+#endif
+
+   if (portActive)                  /* Was the port already active?     */
+      closeline();                  /* Yes --> Shutdown it before open */
+
+   printmsg(15, "pactiveopenline: %s", name);
+
+/*--------------------------------------------------------------------*/
+/*                          Perform the open                          */
+/*--------------------------------------------------------------------*/
+
    rc = DosOpen( name,
-                 &com_handle,
+                 &pipeHandle,
                  &action,
                  0L,
                  0 ,
@@ -142,30 +237,14 @@ int popenline(char *name, BPS baud, const boolean direct )
 
    if ( rc == ERROR_SHARING_VIOLATION)
    {
-      printmsg(0,"Port %s already in use", name);
+      printmsg(0,"Pipe %s already in use", name);
       return TRUE;
    }
-   else if ( rc != 0 )
+   else if ( rc )
    {
-      printmsg(0,"popenline: DosOpen error %d on pipe %s",
-                  (int) rc, name );
+      printOS2error("DosOpen", rc );
       return TRUE;
    }
-
-/*--------------------------------------------------------------------*/
-/*            Reset any errors on the communications port             */
-/*--------------------------------------------------------------------*/
-
-   if (rc)
-   {
-      printmsg(0,
-            "openline: Unable to read errors for %s, error bits %x",
-               name, (int) com_error );
-      printmsg(0,"Return code from DosDevIOCtl was %#04x (%d)",
-               (int) rc , (int) rc);
-   } /*if */
-   else if ( com_error )
-      ShowError( com_error );
 
 /*--------------------------------------------------------------------*/
 /*                           Set baud rate                            */
@@ -176,51 +255,17 @@ int popenline(char *name, BPS baud, const boolean direct )
    traceStart( name );     // Enable logging
 
    portActive = TRUE;      /* Record status for error handler        */
+   passive    = FALSE;
    carrierDetect = FALSE;  /* Modem is not connected                 */
 
-/*--------------------------------------------------------------------*/
-/*                     Up our processing priority                     */
-/*--------------------------------------------------------------------*/
-
-#ifdef __OS2__
-   rc = DosGetInfoBlocks( &ptib, &ppib);
-   if ( !rc ) usPrevPriority = (ptib->tib_ptib2)->tib2_ulpri;
-#else
-   rc = DosGetPrty(PRTYS_PROCESS, &usPrevPriority, 0);
-#endif
-
-   if (rc)
-   {
-      printmsg(0,"openline: Unable to get priority for task");
-      printmsg(0,"Return code from DosGetPrty was %#04x (%d)",
-               (int) rc , (int) rc);
-      panic();
-   } /*if */
-
-   rc = DosSetPrty(PRTYS_PROCESS, priority, prioritydelta, 0);
-
-   if (rc)
-   {
-      printmsg(0,"openline: Unable to set priority %u,%u for task",
-                   priority, prioritydelta);
-
-      printmsg(0,"Return code from DosSetPrty was %#04x (%d)",
-               (int) rc , (int) rc);
-   } /*if */
-
-/*--------------------------------------------------------------------*/
-/*                     Wait for port to stablize                      */
-/*--------------------------------------------------------------------*/
-
-   ddelay(500);            /* Allow port to stablize          */
    return 0;
 
-} /*openline*/
+} /* pactiveopenline */
 
 /*--------------------------------------------------------------------*/
 /*    p s r e a d                                                     */
 /*                                                                    */
-/*    Read from the serial port                                       */
+/*    Read from the pipe                                              */
 /*                                                                    */
 /*   Non-blocking read essential to "g" protocol.  See "dcpgpkt.c"    */
 /*   for description.                                                 */
@@ -242,12 +287,8 @@ unsigned int psread(char *output, unsigned int wanted, unsigned int timeout)
    static USHORT bufsize = 0;
    time_t stop_time ;
    time_t now ;
+   unsigned short wait = 0;
    boolean firstPass = TRUE;
-
-#ifdef __OS2__
-   ULONG ParmLengthInOut;
-   ULONG DataLengthInOut;
-#endif
 
 /*--------------------------------------------------------------------*/
 /*           Determine if our internal buffer has the data            */
@@ -282,11 +323,9 @@ unsigned int psread(char *output, unsigned int wanted, unsigned int timeout)
 
    do {
       USHORT needed =  (USHORT) wanted - bufsize;
-      ULONG status;
 
 #ifdef __OS2__
       ULONG received = 0;
-      AVAILDATA available;
 #else
       USHORT received = 0;
 #endif
@@ -306,53 +345,27 @@ unsigned int psread(char *output, unsigned int wanted, unsigned int timeout)
          return 0;
       }
 
-      if ( firstPass )
+       if ( firstPass )
          firstPass = FALSE;
-      else
-         ddelay(0);
+       else {
+         ddelay(wait);
+         if ( wait < 1000 )
+            wait ++;
+       }
 
 /*--------------------------------------------------------------------*/
-/*                   See if we have the needed data                   */
+/*                 Read the data from the named pipe                  */
 /*--------------------------------------------------------------------*/
 
-      rc = DosPeekNmPipe( com_handle,
-                          &save[bufsize],
-                          needed,
-                          &received,
-                          &available,
-                          &status );
+      rc = DosRead( pipeHandle, &save[bufsize], needed, &received );
 
-      if ( status != NP_STATE_CONNECTED )
+      if ( rc == ERROR_NO_DATA)
+         received = 0;
+      else if ( rc )
       {
-         carrierDetect = FALSE;
-         printmsg(0,"psread: Pipe lost");
-         return 0;
-      }
-
-/*--------------------------------------------------------------------*/
-/*         If not enough data, look through waiting for more          */
-/*--------------------------------------------------------------------*/
-
-      if ( received == 0 )
-         continue;
-
-/*--------------------------------------------------------------------*/
-/*                 Read the data from the serial port                 */
-/*--------------------------------------------------------------------*/
-
-      rc = DosRead( com_handle, &save[bufsize], needed, &received );
-
-      if ( rc == ERROR_INTERRUPT)
-      {
-         printmsg(2,"Read Interrupted");
-         return 0;
-      }
-      else if ( rc != 0 )
-      {
-         printmsg(0,"psread: Read from comm port for %d bytes failed.",
+         printmsg(0,"psread: Read from pipe for %d bytes failed.",
                   needed);
-         printmsg(0,"Return code from DosRead was %#04x (%d)",
-                  (int) rc , (int) rc);
+         printOS2error("DosRead", rc );
          bufsize = 0;
          return 0;
       }
@@ -399,7 +412,7 @@ unsigned int psread(char *output, unsigned int wanted, unsigned int timeout)
 
    return bufsize;
 
-} /*nsread*/
+} /* nsread */
 
 /*--------------------------------------------------------------------*/
 /*    p s w r i t e                                                   */
@@ -411,6 +424,7 @@ int pswrite(const char *input, unsigned int len)
 {
 
    char *data = (char *) input;
+   unsigned int left = len;
 
 #ifdef __OS2__
     ULONG bytes;
@@ -420,20 +434,25 @@ int pswrite(const char *input, unsigned int len)
 
    APIRET rc;
 
-   hangupNeeded = TRUE;      /* Flag that the port is now dirty  */
+   hangupNeeded = TRUE;      /* Flag that the pipe is now dirty  */
 
 /*--------------------------------------------------------------------*/
 /*         Write the data out as the queue becomes available          */
 /*--------------------------------------------------------------------*/
 
-   rc = DosWrite( com_handle, data , len, &bytes);
-   if (rc)
-   {
-      printmsg(0,"nswrite: Write to communications port failed.");
-      printmsg(0,"Return code from DosWrite was %#04x (%d)",
-               (int) rc , (int) rc);
-      return bytes;
-   } /*if */
+   do {
+
+      rc = DosWrite( pipeHandle, data + len - left, left, &bytes);
+
+      if (rc)
+      {
+         printOS2error("DosWrite", rc );
+         return bytes;
+      } /*if */
+
+      left -= bytes;
+
+   } while( left > 0 );
 
 /*--------------------------------------------------------------------*/
 /*                        Log the data written                        */
@@ -442,45 +461,43 @@ int pswrite(const char *input, unsigned int len)
    traceData( data, len, TRUE);
 
 /*--------------------------------------------------------------------*/
-/*            Return bytes written to the port to the caller          */
+/*            Return bytes written to the pipe to the caller          */
 /*--------------------------------------------------------------------*/
 
    return len;
 
-} /*nswrite*/
+} /* nswrite */
+
+#ifdef __TURBOC__
+#pragma argsused
+#endif
 
 /*--------------------------------------------------------------------*/
 /*    p s s e n d b r k                                               */
 /*                                                                    */
-/*    send a break signal out the serial port                         */
+/*    send a break signal out the pipe                                */
 /*--------------------------------------------------------------------*/
 
 void pssendbrk(unsigned int duration)
 {
    printmsg(0,"pssendbrk: BREAK not supported with named pipes");
-} /*nssendbrk*/
+} /* nssendbrk */
 
 /*--------------------------------------------------------------------*/
 /*    p c l o s e l i n e                                             */
 /*                                                                    */
-/*    Close the serial port down                                      */
+/*    Close the named pipe down                                       */
 /*--------------------------------------------------------------------*/
 
 void pcloseline(void)
 {
    APIRET rc;
-   USHORT com_error;
-
-#ifdef __OS2__
-   ULONG ParmLengthInOut;
-   ULONG DataLengthInOut;
-#endif
 
    if ( ! portActive )
       panic();
 
-   portActive = FALSE; /* flag port closed for error handler  */
-   hangupNeeded = FALSE;  /* Don't fiddle with port any more  */
+   portActive = FALSE; /* flag pipe closed for error handler  */
+   hangupNeeded = FALSE;  /* Don't fiddle with pipe any more  */
 
 /*--------------------------------------------------------------------*/
 /*                           Lower priority                           */
@@ -490,20 +507,16 @@ void pcloseline(void)
                    usPrevPriority >> 8 ,
                    usPrevPriority & 0xff, 0);
    if (rc)
-   {
-      printmsg(0,"closeline: Unable to set priority for task");
-      printmsg(0,"Return code from DosSetPrty was %#04x (%d)",
-               (int) rc , (int) rc);
-   } /*if */
+      printOS2error("DosSetPrty", rc );
 
 /*--------------------------------------------------------------------*/
-/*                      Actually close the port                       */
+/*                      Actually close the pipe                       */
 /*--------------------------------------------------------------------*/
 
-   rc = DosClose( com_handle );
+   rc = DosClose( pipeHandle );
 
-   if ( rc != 0 )
-      printmsg( 0,"Close of serial port failed, reason %d", (int) rc);
+   if ( rc )
+      printOS2error("DosClose", rc );
 
 /*--------------------------------------------------------------------*/
 /*                   Stop logging the data to disk                    */
@@ -521,14 +534,20 @@ void pcloseline(void)
 
 void phangup( void )
 {
+   if ( passive && hangupNeeded)
+   {
+      DosDisConnectNPipe( pipeHandle );
+      hangupNeeded = FALSE;
+   }
+
    return;
+
 } /* phangup */
 
 /*--------------------------------------------------------------------*/
 /*    p S I O S p e e d                                               */
 /*                                                                    */
-/*    Re-specify the speed of an opened serial port; no operation on  */
-/*    network.                                                        */
+/*    Re-specify the speed of an opened pipe; no operation, actually. */
 /*--------------------------------------------------------------------*/
 
 #ifdef __TURBOC__
@@ -576,8 +595,6 @@ BPS pGetSpeed( void )
 
 boolean pCD( void )
 {
-   boolean previousCarrierDetect;
-
    return carrierDetect;
 
 } /* pCD */
