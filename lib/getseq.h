f74cac8888a1852bdfba6aa80c5a868ecc16cfde
Drew Derbyshire <ahd@kew.com>
1993-12-02 01:37:33 +0000

    Force system names to seven characters with trailing 0 (grade) for compatbility with Taylor UUCP

--- lib/getseq.h
+++ lib/getseq.h
@@ -1,11 +1,27 @@
 /*--------------------------------------------------------------------*/
-/*    g e t s e q . h                                                 */
+/*       g e t s e q . h                                              */
 /*                                                                    */
-/*    Header file for get sequence and related functions              */
+/*       Header file for get sequence and related functions           */
 /*--------------------------------------------------------------------*/
 
+/*--------------------------------------------------------------------*/
+/*       Changes Copyright (c) 1989-1993 by Kendra Electronic         */
+/*       Wonderworks.                                                 */
+/*                                                                    */
+/*       All rights reserved except those explicitly granted by       */
+/*       the UUPC/extended license agreement.                         */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*                          RCS Information                           */
+/*--------------------------------------------------------------------*/
+
+/*
+ *    $Id: lib.h 1.16 1993/11/06 17:57:46 rhg Exp $
+ */
+
 #define SPOOLFMT "%c.%.8s%c%3.3s"
-#define DATAFFMT "%c.%.8s%3.3s%c"
+#define DATAFFMT "%c.%.7s0%3.3s%c"
 
 long getseq( void );
 
