dfc37c212dfa64d2abd1d62958f0ebefff7dc026
Drew Derbyshire <ahd@kew.com>
1994-03-07 06:09:51 +0000

    Initial revision

---
+++ uucico/catcheru.c
@@ -0,0 +1,29 @@
+/*--------------------------------------------------------------------*/
+/*       c a t c h e r . u                                            */
+/*                                                                    */
+/*       Signal handler for UUPC/extended UUCICO module under         */
+/*       Windows NT.                                                  */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*       Changes Copyright (c) 1989-1994 by Kendra Electronic         */
+/*       Wonderworks.                                                 */
+/*                                                                    */
+/*       All rights reserved except those explicitly granted by       */
+/*       the UUPC/extended license agreement.                         */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*                          RCS Information                           */
+/*--------------------------------------------------------------------*/
+
+/*
+ *    $Id: lib.h 1.20 1994/02/20 19:16:21 ahd Exp $
+ *
+ *    Revision history:
+ *    $Log: lib.h $
+ */
+
+#define UUCICO             /* Perform special processing for Com ports  */
+
+#include "catcher.c"
