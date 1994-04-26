c2add8b106381c1b47f10193cfc60840856d293e
Drew Derbyshire <ahd@kew.com>
1994-04-26 03:32:25 +0000

    Don't print status if hot handle on command line (speeds up TCP/IP starts from INETD)

--- scripts/uuio.cmd
+++ scripts/uuio.cmd
@@ -1,5 +1,19 @@
-/* Rexx */
+/*
+ *       $Id$
+ *
+ *       $Log$
+ */
+
+/*--------------------------------------------------------------------*/
+/*       Copyright 1990-1994 By Kendra Electronic Wonderworks;        */
+/*       may be distributed freely if original documentation and      */
+/*       source are included, and credit is given to the authors.     */
+/*       For additional instructions, see README.PRN in UUPC/         */
+/*       extended documentation archive.                              */
+/*--------------------------------------------------------------------*/
 '@echo off'
-'uustat -q'
+parse arg '-h' foo .
+if foo == '' then
+   'uustat -q'
 'uucico' arg(1)
 'uuxqt'
