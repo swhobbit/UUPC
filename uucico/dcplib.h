95be0e5dd5f84ce006e3dbacadf1bb6f3ea2652a
Drew Derbyshire <ahd@kew.com>
1994-05-06 03:58:32 +0000

    Add copyright

--- uucico/dcplib.h
+++ uucico/dcplib.h
@@ -1,9 +1,28 @@
-/*
-   dcplib.h
+/*--------------------------------------------------------------------*/
+/* d c p l i b . h                                                    */
+/*                                                                    */
+/* System dependent (non-communications related) routines for         */
+/* UUPC/extended.                                                     */
+/*--------------------------------------------------------------------*/
 
-   System dependent (non-communications related) routines for
-   UUPC.
-*/
+/*--------------------------------------------------------------------*/
+/*       Changes Copyright (c) 1989-1994 by Kendra Electronic         */
+/*       Wonderworks.                                                 */
+/*                                                                    */
+/*       All rights reserved except those explicitly granted by       */
+/*       the UUPC/extended license agreement.                         */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*                          RCS Information                           */
+/*--------------------------------------------------------------------*/
+
+/*
+ *    $Id: lib.h 1.21 1994/04/24 20:17:29 dmwatt Exp $
+ *
+ *    Revision history:
+ *    $Log: lib.h $
+ */
 
 boolean login(void);
 
