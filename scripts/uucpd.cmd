1c776178fe3caaf9eb898261e79553418039676c
Drew Derbyshire <ahd@kew.com>
1994-05-09 02:13:02 +0000

    Don't display started command

--- scripts/uucpd.cmd
+++ scripts/uucpd.cmd
@@ -1,7 +1,10 @@
-start /min /n uucico -h %1 -m tcpip -U
 @echo off
-rem      $Id: uucpd.cmd 1.1 1994/04/26 03:32:25 ahd Exp ahd $
+start /min /n uucico -h %1 -m tcpip -U
+rem      $Id: uucpd.cmd 1.2 1994/04/27 00:05:45 ahd Exp ahd $
 rem
 rem      $Log: uucpd.cmd $
+rem     Revision 1.2  1994/04/27  00:05:45  ahd
+rem     Automatically run UUXQT after completion
+rem
 rem     Revision 1.1  1994/04/26  03:32:25  ahd
 rem     Initial revision
