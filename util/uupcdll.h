3c290de84a17ddf6ea683366d4df5b7d04962ee1
Drew Derbyshire <ahd@kew.com>
1994-05-30 02:23:52 +0000

    Defines for UUPCDLL.DLL

--- util/uupcdll.h
+++ util/uupcdll.h
@@ -21,9 +21,13 @@
 /*--------------------------------------------------------------------*/
 /*                          RCS Information                           */
 /*--------------------------------------------------------------------*/
+
 /*
- *       $Id: uupcdll.h 1.1 1994/05/21 14:12:03 dmwatt Exp $
+ *       $Id: uupcdll.h 1.1 1994/05/23 22:49:49 ahd Exp $
  *       $Log: uupcdll.h $
+ * Revision 1.1  1994/05/23  22:49:49  ahd
+ * Initial revision
+ *
  *
  */
 
@@ -31,31 +35,31 @@
 
 BOOL UUPCInit(void);
 /*
-	UUPCInit() initializes the UUPC DLL.  (At the moment, it does
-	nothing.)
+        UUPCInit() initializes the UUPC DLL.  (At the moment, it does
+        nothing.)
 */
 
 BOOL UUPCGetParm(char *parmName, char *buf, int len);
 /*
-	UUPCGetParm() gets the UUPC system parameter named parmName out
-	of the registry.  It only works if you've run REGSETUP -s on a
-	correctly configured UUPC/extended system first.
+        UUPCGetParm() gets the UUPC system parameter named parmName out
+        of the registry.  It only works if you've run REGSETUP -s on a
+        correctly configured UUPC/extended system first.
 */
 
 BOOL UUPCGetNewsSpoolSize(char *system, long *count, long *bytes);
 /*
-	Returns the total number of files and their total size in the 'D'
-	directory under SpoolDir for the given system.
+        Returns the total number of files and their total size in the 'D'
+        directory under SpoolDir for the given system.
 */
 
 BOOL UUPCSendMail(char *message);
 /*
-	Sends a message to a user using UUPC's RMAIL program.  The message
-	must have at least a To: line and a From: line, followed by two
-	newlines, in order for UUPCSendMail to consider it a valid message.
-	It does <no> further checking for the validity of the message itself,
-	but will return an error if RMAIL had a problem delivering it for
-	some reason.
+        Sends a message to a user using UUPC's RMAIL program.  The message
+        must have at least a To: line and a From: line, followed by two
+        newlines, in order for UUPCSendMail to consider it a valid message.
+        It does <no> further checking for the validity of the message itself,
+        but will return an error if RMAIL had a problem delivering it for
+        some reason.
 */
 
 #endif
