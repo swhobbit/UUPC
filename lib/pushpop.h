4f2a882d79f8d8f5ba24868e502064b7906cdb54
Drew Derbyshire <ahd@kew.com>
1995-02-14 05:06:13 +0000

    Correct NT directory processing

--- lib/pushpop.h
+++ lib/pushpop.h
@@ -4,6 +4,26 @@
 /*    Functions defined in pushpop.c for UUPC/extended                */
 /*--------------------------------------------------------------------*/
 
+/*--------------------------------------------------------------------*/
+/*       Changes Copyright (c) 1989-1995 by Kendra Electronic         */
+/*       Wonderworks.                                                 */
+/*                                                                    */
+/*       All rights reserved except those explicitly granted by       */
+/*       the UUPC/extended license agreement.                         */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*                          RCS Information                           */
+/*--------------------------------------------------------------------*/
+
+/*
+ *    $Id: lib.h 1.31 1995/02/12 23:39:45 ahd Exp $
+ *
+ *    Revision history:
+ *    $Log: lib.h $
+ */
+
+
 void PushDir( const char *directory );
 
 void PopDir( void );
