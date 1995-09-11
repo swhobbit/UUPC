987353581ef491f16ea1b6614e1137f793823af7
Drew Derbyshire <ahd@kew.com>
1995-09-11 00:24:49 +0000

    Bring in line with current header style

--- lib/arpadate.h
+++ lib/arpadate.h
@@ -1,11 +1,26 @@
+/*--------------------------------------------------------------------*/
+/*       a r p a d a t e . h                                          */
+/*                                                                    */
+/*       RFC-822 (Internet mail header) date header function          */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*       Changes Copyright (c) 1989-1995 by Kendra Electronic         */
+/*       Wonderworks.                                                 */
+/*                                                                    */
+/*       All rights reserved except those explicitly granted by       */
+/*       the UUPC/extended license agreement.                         */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*                          RCS Information                           */
+/*--------------------------------------------------------------------*/
+
 /*
-      Arpadate.h
-
-      Function header for arpadate.c
-
-      Change History:
-
-      09 Sep 90   -  Split from pcmail.c                             ahd
+ *    $Id: lib.h 1.32 1995/07/21 13:28:20 ahd v1-12o $
+ *
+ *    Revision history:
+ *    $Log: lib.h $
  */
 
 char *arpadate( void );          /* Returns current date in static
