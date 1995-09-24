384c7c3a025864a6fca6312bcb0012c5647fe4dc
Drew Derbyshire <ahd@kew.com>
1995-09-24 19:11:38 +0000

    Use const parameters where possible

--- lib/getseq.h
+++ lib/getseq.h
@@ -17,9 +17,12 @@
 /*--------------------------------------------------------------------*/
 
 /*
- *    $Id: getseq.h 1.5 1994/12/22 00:12:52 ahd Exp $
+ *    $Id: getseq.h 1.6 1995/02/12 23:39:45 ahd v1-12o $
  *
  *    $Log: getseq.h $
+ *    Revision 1.6  1995/02/12 23:39:45  ahd
+ *    compiler cleanup, NNS C/news support, optimize dir processing
+ *
  *    Revision 1.5  1994/12/22 00:12:52  ahd
  *    Annual Copyright Update
  *
@@ -33,6 +36,6 @@
 
 long getSeq( void );
 
-char *jobNumber( long sequence,
-                 size_t length,
+char *jobNumber( const long sequence,
+                 const size_t length,
                  const KWBoolean monocase );
