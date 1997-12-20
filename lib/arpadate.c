2ef1a0e544321de56680cc27992c85493a719932
Drew Derbyshire <ahd@kew.com>
1997-12-20 22:26:06 +0000

    Support whacko compilers that think time_t is unsigned

--- lib/arpadate.c
+++ lib/arpadate.c
@@ -22,58 +22,22 @@
 /*--------------------------------------------------------------------*/
 
 /*
- *    $Id: arpadate.c 1.15 1996/01/01 20:49:17 ahd v1-12r $
+ *    $Id: arpadate.c 1.16 1997/03/31 06:58:04 ahd v1-12u $
  *
  *    Revision history:
  *    $Log: arpadate.c $
+ *    Revision 1.16  1997/03/31 06:58:04  ahd
+ *    Annual Copyright Update
+ *
  *    Revision 1.15  1996/01/01 20:49:17  ahd
  *    Annual Copyright Update
- *
- *    Revision 1.14  1994/12/22 00:07:16  ahd
- *    Annual Copyright Update
- *
- *    Revision 1.13  1994/02/19 04:48:05  ahd
- *    Use standard first header
- *
- *     Revision 1.12  1994/02/19  04:01:16  ahd
- *     Use standard first header
- *
- *     Revision 1.11  1994/02/18  23:16:55  ahd
- *     Use standard first header
- *
- *     Revision 1.10  1994/02/14  01:03:56  ahd
- *     Drop unneeded currentfile() macro
- *
- *     Revision 1.9  1994/02/13  13:52:33  rommel
- *     Use shorter logic  for computing TZ offset
- *
- *     Revision 1.8  1994/01/01  19:00:21  ahd
- *     Annual Copyright Update
- *
- *     Revision 1.7  1993/11/30  04:18:14  ahd
- *     Correct for TZ computations crossing day boundaries
- *
- *     Revision 1.7  1993/11/30  04:18:14  ahd
- *     Correct for TZ computations crossing day boundaries
- *
- *     Revision 1.6  1993/11/15  05:43:29  ahd
- *     Twiddle, twiddle ...
- *
- *     Revision 1.5  1993/11/13  17:37:02  ahd
- *     More date twiddling
- *
- *     Revision 1.4  1993/11/06  16:56:13  ahd
- *     Use localtime to determine tz offset
- *
- *     Revision 1.3  1993/10/28  00:18:10  ahd
- *     Apply Kai Uwe Rommel's fix to use TZ offset in hours rather than
- *     time zone
  *
  */
 
 #include "uupcmoah.h"
-
 #include "arpadate.h"
+
+RCSID("$Id$");
 
 /*--------------------------------------------------------------------*/
 /*       a r p a d a t e                                              */
@@ -96,10 +60,10 @@ char *arpadate( void )
 /*--------------------------------------------------------------------*/
 
    if (zone[0] == 0)              /* adjust for daylight savings time */
-            {
+   {
       gm = *gmtime(&t);
       ut = mktime(&gm);
-      sprintf(zone, " %+03d00", (t - ut) / 3600);
+      sprintf(zone, " %+03ld00", (((long) t) - (long) ut) / 3600);
    }
 
 /*--------------------------------------------------------------------*/
