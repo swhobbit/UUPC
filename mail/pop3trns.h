c6d1cbd81d60bb81f2785e4228e8d26f5e37b82e
Drew Derbyshire <ahd@kew.com>
1998-03-03 03:55:26 +0000

    First POP3 server to handle most data commands

--- mail/pop3trns.h
+++ mail/pop3trns.h
@@ -20,20 +20,43 @@
 /*--------------------------------------------------------------------*/
 
 /*
- *       $Id: smtptrns.h 1.3 1998/03/01 01:28:04 ahd Exp $
+ *       $Id: pop3trns.h 1.1 1998/03/01 19:45:01 ahd Exp $
  *
  *       Revision history:
- *       $Log: smtptrns.h $
+ *       $Log: pop3trns.h $
+ *       Revision 1.1  1998/03/01 19:45:01  ahd
+ *       Initial revision
+ *
  */
 
 #include "usertabl.h"
 #include "imfile.h"
 
+typedef struct _MailMessage
+{
+   long sequence;                   /* Message num, counting from 1  */
+   long octets;                     /* Size in bytes                 */
+   long startPosition;
+   long startBodyPosition;
+   long endPosition;
+   char *uidl;                      /* unique string to indent msg   */
+   KWBoolean deleted;
+   KWBoolean fakeUIDL;              /* UIDL was generated on the fly */
+   struct _MailMessage *previous;
+   struct _MailMessage *next;
+
+} MailMessage;
+
 typedef struct _POP3Transaction
 {
 
+   long messageCount;               /* Mailbox message count         */
+   long octets;                     /* Mailbox size in bytes         */
+   char mailboxName[FILENAME_MAX];  /* Box we move data into         */
    struct UserTable *userp;
    IMFILE *imf;                     /* Mailbox temporary copy        */
+   FILE *mailboxStream;             /* Stream used to access mbox    */
+   MailMessage *top;                /* Our message queue             */
 
 } POP3Transaction;
 
