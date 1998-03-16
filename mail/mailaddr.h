7f1b1e33cc6d0478882e7ea78a8600ffa4430028
Drew Derbyshire <ahd@kew.com>
1998-03-16 06:43:00 +0000

    Better trumpet support in rmail

--- mail/mailaddr.h
+++ mail/mailaddr.h
@@ -22,9 +22,12 @@
 /*--------------------------------------------------------------------*/
 
 /*
- *    $Id: mailaddr.h 1.2 1998/03/01 01:30:57 ahd v1-12v $
+ *    $Id: mailaddr.h 1.3 1998/03/08 23:12:28 ahd Exp $
  *
  *    $Log: mailaddr.h $
+ *    Revision 1.3  1998/03/08 23:12:28  ahd
+ *    Add new fields for local information
+ *
  *    Revision 1.2  1998/03/01 01:30:57  ahd
  *    Annual Copyright Update
  *
@@ -41,6 +44,7 @@ typedef struct _MAIL_ADDR
    char *user;                   /* Mailbox of sender address        */
    char *activeUser;             /* Local user executing program     */
    KWBoolean remote;             /* Address is non-local             */
+   KWBoolean daemon;             /* Delivery by long running daemon  */
 }  MAIL_ADDR;
 
 #endif /* _MAILADDR_H */
