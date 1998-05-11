c532541a9413fc20fe912c67b432709790cc41b5
Drew Derbyshire <ahd@kew.com>
1998-05-11 01:25:40 +0000

    Make copylog public

--- lib/logger.h
+++ lib/logger.h
@@ -1,14 +1,34 @@
+#ifndef _LOGGER_H
+#define _LOGGER_H
 /*--------------------------------------------------------------------*/
 /*    l o g g e r . h                                                 */
 /*                                                                    */
 /*    Logging functions for UUPC/extended                             */
-/*                                                                    */
-/*    Copyright (c) 1992 by Kendra Electronic Wonderworks; all        */
-/*    rights reserved except those explicitly granted by the          */
-/*    UUPC/extended license.                                          */
 /*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*       Changes Copyright (c) 1989-1998 by Kendra Electronic         */
+/*       Wonderworks.                                                 */
+/*                                                                    */
+/*       All rights reserved except those explicitly granted by       */
+/*       the UUPC/extended license agreement.                         */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*                          RCS Information                           */
+/*--------------------------------------------------------------------*/
+
+/*
+ *       $Id: lib.h 1.44 1998/04/27 01:57:29 ahd v1-13a $
+ *
+ *       Revision history:
+ *       $Log: lib.h $
+ */
 
 void openlog( const char *log );
 
+void copylog(void);
+
 extern char *full_log_file_name;
 
+#endif /* _LOGGER_H */
